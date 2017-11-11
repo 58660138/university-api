caadce61e3c8bf118d15ea589296c9eb4e1f0e4f
warintorn yangkijkarn <58660138@go.buu.ac.th>
2017-11-11 12:08:26 +0700

    try to create a api

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+
+
+let students = [
+{ id : 1,name:'Weera',email :'ball@outlook.com'},
+{ id : 2,name:'Thannabodee',email:'win@outlook.com'}
+]
+
+app.post('/students',(req,res) => {
+  let student = req.body
+  students.push(student)
+  res.json(student)
+})
+
+app.get('/greeting',(req,res)=> {
+  res.json({ message:'Hello'})
+})
+
+app.get('/students',(req,res)=>{
+  res.json(students)
+})
+
+
+
+module.exports = app
